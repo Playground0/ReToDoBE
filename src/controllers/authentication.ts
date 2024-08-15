a59404f3cdd0cc38758b687b39c686c2a2b4b0d2
Playground0 <emailplayg1@gmail.com>
2024-08-15 20:58:21 +0530

    add reset-functionality

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -1,5 +1,9 @@
 import express from "express";
-import { createUser, getUserByEmail } from "../models/schemas/users";
+import {
+  createUser,
+  getUserByEmail,
+  getUserById,
+} from "../models/schemas/users";
 import { authentication, random } from "../helpers";
 import {
   interalServerError,
@@ -8,6 +12,9 @@ import {
 } from "../utils/error-message-handler";
 import { APIResponse } from "../models/api-response.model";
 import { APIStatusCode } from "../models/constants/status.constants";
+import jwt from "jsonwebtoken";
+import nodemailer from "nodemailer";
+import { MailOptions } from "nodemailer/lib/json-transport";
 
 export const register = async (req: express.Request, res: express.Response) => {
   // TODO: Create model for user and assign it
@@ -31,7 +38,9 @@ export const register = async (req: express.Request, res: express.Response) => {
   try {
     const existingUser = await getUserByEmail(email);
     if (existingUser) {
-      return res.status(APIStatusCode.Conflict).json(APIResponse.error(action, "User Exist", APIStatusCode.Conflict));
+      return res
+        .status(APIStatusCode.Conflict)
+        .json(APIResponse.error(action, "User Exist", APIStatusCode.Conflict));
     }
 
     const salt = random();
@@ -84,14 +93,26 @@ export const login = async (req: express.Request, res: express.Response) => {
     if (!user.authentication?.salt) {
       return res
         .status(APIStatusCode.Forbidden)
-        .json(APIResponse.error(action, "Authentication Failed", APIStatusCode.Forbidden));
+        .json(
+          APIResponse.error(
+            action,
+            "Authentication Failed",
+            APIStatusCode.Forbidden
+          )
+        );
     }
 
     const expectedHash = authentication(user.authentication.salt, password);
     if (expectedHash !== user.authentication.password) {
       return res
         .status(APIStatusCode.Unauthorized)
-        .json(APIResponse.error(action, "Please check your password", APIStatusCode.Unauthorized));
+        .json(
+          APIResponse.error(
+            action,
+            "Please check your password",
+            APIStatusCode.Unauthorized
+          )
+        );
     }
 
     const salt = random();
@@ -107,7 +128,10 @@ export const login = async (req: express.Request, res: express.Response) => {
       userRole: user.userRole,
     };
 
-    return res.status(APIStatusCode.OK).json(APIResponse.success(responseBody, action)).end();
+    return res
+      .status(APIStatusCode.OK)
+      .json(APIResponse.success(responseBody, action))
+      .end();
   } catch (error) {
     return interalServerError(action, error, res);
   }
@@ -141,8 +165,138 @@ export const logout = async (req: express.Request, res: express.Response) => {
       (token) => token !== sessionToken
     );
     user.save();
-    return res.status(APIStatusCode.NoContent).json(APIResponse.success([], action,APIStatusCode.NoContent)).end();
+    return res
+      .status(APIStatusCode.OK)
+      .json(APIResponse.success([], action, APIStatusCode.OK))
+      .end();
   } catch (error) {
+    return interalServerError(action, error, res);
+  }
+};
+
+export const forgotPassword = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  const { email, url } = req.body;
+  const action = "Forgot Password";
+
+  try {
+    const user = await getUserByEmail(email);
+
+    if (!user) {
+      return notFoundError(action, "User not found", res);
+    }
+
+    const secret = process.env.JWT_SECRET;
+    const token = jwt.sign({ id: user._id }, secret!, {
+      expiresIn: "15m",
+    });
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+
+    const mailOptions: MailOptions = {
+      from: process.env.EMAIL,
+      to: user.email,
+      subject: "Password Reset",
+      text: `You requested for a password reset. Please click this link to reset your password: ${url}/reset-password/${token}`,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        return res
+          .status(APIStatusCode.InternalServalError)
+          .json(
+            APIResponse.error(
+              action,
+              error.message,
+              APIStatusCode.InternalServalError
+            )
+          );
+      }
+      res
+        .status(APIStatusCode.OK)
+        .json(
+          APIResponse.success(
+            [],
+            action,
+            APIStatusCode.OK,
+            "Password reset link sent to your email"
+          )
+        );
+    });
+  } catch (error) {
+    return interalServerError(action, error, res);
+  }
+};
+
+export const resetPassword = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  const {token, newPassword} = req.body;
+  const action = "Reset Password";
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!);
+    const userId = (decoded as any).id;
+    const user = await getUserById(userId).select(
+      "+authentication.salt +authentication.password +authentication.sessionToken"
+    );
+
+    if (!user) {
+      return notFoundError(action, "Invalid token or User not found", res);
+    }
+
+    const salt = random();
+
+    user.authentication = {
+      salt,
+      password: authentication(salt, newPassword),
+      sessionToken: [],
+    };
+
+    await user.save();
+    return res
+      .status(APIStatusCode.OK)
+      .json(
+        APIResponse.success(
+          [],
+          action,
+          APIStatusCode.OK,
+          "Password Changed Successfully"
+        )
+      );
+  } catch (error: Error | any | unknown) {
+    if (error.name === "TokenExpiredError") {
+      return res
+        .status(APIStatusCode.BadRequest)
+        .json(
+          APIResponse.error(
+            action,
+            "Password reset link has expired. Please request a new one.",
+            APIStatusCode.BadRequest,
+            error.name
+          )
+        );
+    } else if (error.name === "JsonWebTokenError") {
+      return res
+        .status(APIStatusCode.BadRequest)
+        .json(
+          APIResponse.error(
+            action,
+            "Invalid password reset link.",
+            APIStatusCode.BadRequest,
+            error.name
+          )
+        );
+    }
     return interalServerError(action, error, res);
   }
 };
